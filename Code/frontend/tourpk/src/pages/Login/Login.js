19de987930912cd0417db9e9b8a522fd9f936e99
Mahnoor <mahnoorali57se@gmail.com>
2023-01-10 02:27:55 +0500

    final form package installation

---
+++ Code/frontend/tourpk/src/pages/Login/Login.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { LoginForm } from "../../components/LoginForm/LoginForm";
+
+const Login = () => {
+
+    return (
+        <>  
+            <NavBar />
+            <LoginForm />
+            
+        
+        </>
+    );
+
+}
+
+export default Login;
