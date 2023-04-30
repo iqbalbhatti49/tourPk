688ca32bddf8389844ad51eef36495a4bfc1ccec
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2023-04-30 15:57:56 +0500

    payment trails

--- Code/server/index.js
+++ Code/server/index.js
@@ -1,28 +1,56 @@
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/authUser");
-const blogRouter = require("./routes/Blogs");
-const helpRouter = require("./routes/Help");
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const db = require("./models");
-const app = express();
-require('dotenv').config();
+// const express = require("express");
+// const cors = require("cors");
+// const authRoutes = require("./routes/authUser");
+// const blogRouter = require("./routes/Blogs");
+// const helpRouter = require("./routes/Help");
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const db = require("./models");
+// const app = express();
+// require('dotenv').config();
 
 
-app.use(bodyParser.json()); // parses the incoming request bodies in a middleware before your handlers, available under the req.body property
-app.use(cookieParser()); // to parse cookies attached to the client request object
+// app.use(bodyParser.json()); // parses the incoming request bodies in a middleware before your handlers, available under the req.body property
+// app.use(cookieParser()); // to parse cookies attached to the client request object
 
-app.use(express.json()); // To parse the incoming requests with JSON payloads
-app.use(cors());
-// set up router for handling HTTP requests related to each route
-app.use("/tourpk", blogRouter); //middleware to handle requests to /Blogs route
-app.use("/tourpk/auth", authRoutes);
-app.use("/help", helpRouter);
+// app.use(express.json()); // To parse the incoming requests with JSON payloads
+// app.use(cors());
+// // set up router for handling HTTP requests related to each route
+// app.use("/tourpk", blogRouter); //middleware to handle requests to /Blogs route
+// app.use("/tourpk/auth", authRoutes);
+// app.use("/help", helpRouter);
 
 
-// const PORT = process.env.PORT || 3000;  --> for production
-db.sequelize.sync().then(() => {
-  app.listen(8080, console.log(`Server running on port 8080`))
-  console.log("aaaaaaaaaaaaa");
+// // const PORT = process.env.PORT || 3000;  --> for production
+// db.sequelize.sync().then(() => {
+//   app.listen(8080, console.log(`Server running on port 8080`))
+//   console.log("aaaaaaaaaaaaa");
+// });
+
+const Jazzcash = require('jazzcash-checkout')
+
+Jazzcash.credentials({
+  config: {
+    merchantId: "MC56301",
+    password: "2v2gybgu29",
+    hashKey: "dyu6w1090w",
+  },
+  environment: 'sandbox'
+});
+
+Jazzcash.setData({
+  pp_Amount: 100,
+  pp_BillReference: "billRef123",
+  pp_Description: "Test Payment",
+  pp_MobileNumber: "03123456789",
+  pp_CNIC: "345678",
+  pp_TxnExpiryDateTime: "20230429205921",
+  pp_TxnDateTime: "20230429205921",
+  pp_TxnType: "MPAY",
+  pp_Version: "2.0",
+});
+
+Jazzcash.createRequest("PAY").then((res) => {
+  res = JSON.parse(res);
+  console.log(res);
 });
--- Code/server/routes/Payment.js
+++ Code/server/routes/Payment.js
@@ -2,9 +2,9 @@ const Jazzcash = require('jazzcash-checkout')
 
 Jazzcash.credentials({
    config: {
-      merchantId: "",
-      password: "",
-      hashKey: "",
+      merchantId: "MC56301",
+      password: "2v2gybgu29",
+      hashKey: "dyu6w1090w",
    },
    environment: 'sandbox'
 });
@@ -17,7 +17,55 @@ Jazzcash.setData({
    pp_CNIC: "345678",
 });
 
-Jazzcash.createRequest("WALLET").then((res) => {
-   res = JSON.parse(res); 
+Jazzcash.createRequest("PAY").then((res) => {
+   res = JSON.parse(res);
    console.log(res);
 });
+
+
+// const axios = require('axios');
+
+// // Request payload
+// const payload = {
+//    "pp_CustomerID": "25352",
+//    "pp_CustomerEmail": "abc@abc.com",
+//    "pp_CustomerMobile": "03331234567",
+//    "pp_Version": "1.1",
+//    "pp_TxnType": "MPAY",
+//    "pp_TxnRefNo": "T20230429205921",
+//    "pp_MerchantID": "MC56301",
+//    "pp_Password": "2v2gybgu29",
+//    "pp_Amount": "1000",
+//    "pp_TxnCurrency": "PKR",
+//    "pp_TxnDateTime": "20230429205921",
+//    "pp_TxnExpiryDateTime": "20230429205921",
+//    "pp_BillReference": "billRef",
+//    "pp_Description": "Description of transaction",
+//    "pp_CustomerCardNumber": "5123450000000008",
+//    "pp_CustomerCardCVV": "100",
+//    "pp_CustomerCardExpiry": "01/39",
+//    "pp_SecureHash": "E4600AB0FB882A271B934913BB750A53FBCBCFBB23B40A4C90FA6890CCE4ED96",
+//    "pp_DiscountedAmount": "",
+//    "pp_DiscountBank": "",
+//    "pp_UsageMode": "API"
+// }
+// // Send request to JazzCash API
+// axios.post('https://sandbox.jazzcash.com.pk/ApplicationAPI/API/Purchase/PAY', payload)
+//    .then(response => {
+//       const responseData = response.data;
+//       // Process the response
+//       if (responseData.responseCode === '000') {
+//          // Payment successful
+//          console.log('Payment successful!');
+//          console.log('Retrieval Reference Number:', responseData.pp_RetreivalReferenceNo);
+//       } else {
+//          // Payment failed
+//          console.log('Payment failed!');
+//          console.log(responseData)
+//          console.log('Error:', responseData.responseMessage);
+//       }
+//    })
+//    .catch(error => {
+//       // An error occurred during the request
+//       console.error('Error:', error.message);
+//    });
