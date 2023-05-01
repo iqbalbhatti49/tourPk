779a547e51cb4dba8899bbb2cf7121f8d121662f
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2023-05-01 22:53:35 +0500

    city card linking

--- Code/frontend/tourpk/src/components/CityCard/CityCard.js
+++ Code/frontend/tourpk/src/components/CityCard/CityCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, ImageHeader, CardBody } from 'react-simple-card';
 import styles from './CityCard.module.css';
+import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 export const CityCard = (props) => {
    const { city } = props;
@@ -11,11 +13,18 @@ export const CityCard = (props) => {
             <div className={styles.body}>
                <p className={styles.title}>{city.tagline}</p>
                <div className={styles.stats}>
-                  <p className={styles.place}>{city.name}</p>
-                  <p className={styles.reviews}>{city.reviews} reviews</p>
+                  <div>
+                     <p className={styles.place}>{city.name}</p>
+                     <p className={styles.reviews}>{city.reviews} reviews</p>
+                  </div>
+                  <Link to={{ pathname: '/services', search: `?city=${city.name}` }}
+                  >
+                     <Button type="secondary" value="Visit Details" />
+                  </Link>
                </div>
             </div>
          </CardBody>
       </Card>
+
    );
 };
--- Code/frontend/tourpk/src/pages/Services/Services.js
+++ Code/frontend/tourpk/src/pages/Services/Services.js
@@ -3,13 +3,19 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { LahoreHotles, LahoreResturants } from "../../utils/FakeData";
 import { IconHotel, IconResturant, IconGuide, IconAgent, HotelCard } from "../../components/index";
+import { useLocation } from "react-router-dom";
 
 const Services = () => {
+   const location = useLocation();
+   console.log(location.search);
    return (
       <div className={styles.container}>
          <div className={styles.header}>
             <p className={styles.subHeading}>{`Our Services`}</p>
             <h1 className={styles.Heading}>Book What You Want</h1>
+            <p className={styles.description}>
+               Do what you can, with what you have, where you are.Do what you can, with what you have, where you are.Do what you can, with what you have, where you are.
+            </p>
          </div>
          <div className={styles.tabs}>
             <Tabs focusTabOnClick={false}>
