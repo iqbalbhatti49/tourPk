bc071d77df9e4b7191f05c5e6e682e8afe8cabf1
Mahnoor <mahnoorali57se@gmail.com>
2023-05-30 00:37:11 +0500

    FormField checkbox update

--- Code/frontend/tourpk/src/components/FormField/FormField.js
+++ Code/frontend/tourpk/src/components/FormField/FormField.js
@@ -6,15 +6,19 @@ let classWrapper = classNames.bind(styles);
 
 const FormField = (props) => {
   const { name, type, placeholder, validate, theme, renderIcon, label, labelClass } = props;
-  let classes = classWrapper({ formField: type != 'checkbox', iconInput: renderIcon() != null, noIconInput: renderIcon() == null, light: theme == 'light' });
-  let labelClassName = classWrapper({ noLabel: labelClass == 'noLabel', showLabel: labelClass != 'noLabel' });
-  labelClassName = type == 'checkbox' ? classWrapper({ checkBoxLabel: true }) : labelClassName;
-  let wrapperClass = classWrapper({ field: type != 'checkbox', checkBoxField: type == 'checkbox' });
+  let classes = classWrapper({ formField: type !== 'checkbox', iconInput: renderIcon() !== null, noIconInput: renderIcon() === null, light: theme === 'light' });
+  let labelClassName = classWrapper({ noLabel: labelClass === 'noLabel', showLabel: labelClass !== 'noLabel' });
+  labelClassName = type === 'checkbox' ? classWrapper({ checkBoxLabel: true }) : labelClassName;
+  let wrapperClass = classWrapper({ field: type !== 'checkbox', checkBoxField: type === 'checkbox' });
+
+  const handleCheckboxChange = (event, input) => {
+    input.onChange(event.target.checked);
+  };
+
   return (
     <Field
       name={name}
       validate={validate}
-      //re-render the component whenever: field becomes active, field has been touched, field's error state or field value changes
       subscription={{
         value: true,
         active: true,
@@ -23,17 +27,39 @@ const FormField = (props) => {
       }}
     >
       {({ input, meta }) => (
-        <div className={wrapperClass} >
-          {renderIcon() != null && <div className={styles.fieldIcon} id={styles.tooltip}>{renderIcon()}
-            {meta.error && meta.touched && <span id={styles.tooltiptext} >{meta.error}</span>}
-          </div>}
-          <label className={labelClassName} id={styles.tooltip}>{label}
-            {meta.error && meta.touched && <span id={styles.tooltiptext} className={styles.error}>{meta.error}</span>}
-          </label>
-          <input className={classes} id={`${meta.error && meta.touched ? styles.error : ''}`} {...input} placeholder={placeholder} type={type} />
+        <div className={wrapperClass}>
+          {type === 'checkbox' ? (
+            <label className={labelClassName} id={styles.checkBoxText}>
+              <input
+                className={styles.checkboxInput}
+                {...input}
+                type="checkbox"
+                checked={input.value}
+                onChange={(event) => handleCheckboxChange(event, input)}
+              />
+              <span className={styles.checkboxCustom}></span>
+              {label}
+              {meta.error && meta.touched && <span id={styles.tooltiptext} className={styles.error}>{meta.error}</span>}
+            </label>
+          ) : (
+            <>
+              {renderIcon() !== null && (
+                <div className={styles.fieldIcon} id={styles.tooltip}>
+                  {renderIcon()}
+                  {meta.error && meta.touched && <span id={styles.tooltiptext}>{meta.error}</span>}
+                </div>
+              )}
+              <label className={labelClassName} id={styles.tooltip}>
+                {label}
+                {meta.error && meta.touched && <span id={styles.tooltiptext} className={styles.error}>{meta.error}</span>}
+              </label>
+              <input className={classes} id={`${meta.error && meta.touched ? styles.error : ''}`} {...input} placeholder={placeholder} type={type} />
+            </>
+          )}
         </div>
       )}
     </Field>
   );
 };
+
 export default FormField;
