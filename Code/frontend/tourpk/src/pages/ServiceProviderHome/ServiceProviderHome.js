d80052cca966e2cab92a95aa212922866190a4c4
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2023-06-09 02:05:12 -0700

    service provider home

---
+++ Code/frontend/tourpk/src/pages/ServiceProviderHome/ServiceProviderHome.js
@@ -0,0 +1,11 @@
+import styles from "./ServiceProviderHome.module.css";
+
+const ServiceProviderHome = () => {
+   return (
+      <div className={styles.home}>
+         <p>Service Provider Home</p>
+      </div>
+   );
+};
+
+export default ServiceProviderHome;
